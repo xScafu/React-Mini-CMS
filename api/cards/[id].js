69b516400b513c7893abdd6a768cbd1ab6141089
xScafu <a.scarlata93@gmail.com>
2026-03-13 16:50:01 +0100

    fix

---
+++ api/cards/[id].js
@@ -0,0 +1,11 @@
+import { getDb } from "../../lib/mongodb.js";
+import { ObjectId } from "mongodb";
+export default async function handler(req, res) {
+    const { id } = req.query;
+    const db = await getDb();
+    const collection = db.collection("cards");
+    if (req.method === "GET") {
+        const card = await collection.findOne({ _id: new ObjectId(id) });
+        return res.status(200).json(card);
+    }
+}
