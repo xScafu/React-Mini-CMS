72c8c05339db66366fba2b052633740e262b4814
xScafu <a.scarlata93@gmail.com>
2026-03-13 16:51:52 +0100

    removed doubled path

--- api/cards/index.js
+++
@@ -1,9 +0,0 @@
-import { getDb } from "../../lib/mongodb.js";
-export default async function handler(req, res) {
-    const db = await getDb();
-    const collection = db.collection("cards");
-    if (req.method === "GET") {
-        const cards = await collection.find().toArray();
-        return res.status(200).json(cards);
-    }
-}
--- api/labels/index.js
+++
@@ -1,9 +0,0 @@
-import { getDb } from "../../lib/mongodb.js";
-export default async function handler(req, res) {
-    const db = await getDb();
-    const collection = db.collection("labels");
-    if (req.method === "GET") {
-        const labels = await collection.find().toArray();
-        return res.status(200).json(labels);
-    }
-}
