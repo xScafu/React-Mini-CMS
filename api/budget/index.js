a199c84fef5f12d2fea8b073d1e7952316ea678c
xScafu <a.scarlata93@gmail.com>
2026-03-13 17:13:08 +0100

    Fix: remove duplicate compiled files

--- api/budget/index.js
+++
@@ -1,9 +0,0 @@
-import { getDb } from "../../lib/mongodb.js";
-export default async function handler(req, res) {
-    const db = await getDb();
-    const collection = db.collection("budget");
-    if (req.method === "GET") {
-        const balance = await collection.find().toArray();
-        return res.status(200).json(balance);
-    }
-}
